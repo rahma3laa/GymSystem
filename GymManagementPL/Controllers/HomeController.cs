ccc74e61d9ac0c7a934a983d58995a48d4a4f2be
rahma3laa <rahmaaaliii55@gmail.com>
2025-10-18 14:24:07 +0300

    V10

--- GymManagementPL/Controllers/HomeController.cs
+++ GymManagementPL/Controllers/HomeController.cs
@@ -1,32 +1,48 @@
-using System.Diagnostics;
-using GymManagementPL.Models;
+﻿using GymManagementDAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //ActionResult
+        public ViewResult Index()
         {
-            _logger = logger;
+          return View();
+
         }
 
-        public IActionResult Index()
+        public JsonResult Trainers()
         {
-            return View();
+            var Trainers = new List<Trainer>()
+            {
+                new Trainer(){ Name ="Rahma" , Phone ="010060"},
+                new Trainer(){ Name ="Ali" , Phone ="01006096175"},
+            };
+            return Json(Trainers);
         }
 
-        public IActionResult Privacy()
+        public RedirectResult Redirect()
         {
-            return View();
+            return Redirect("https://www.bing.com/search?q=download+github+desktop&gs_lcrp=EgRlZGdlKgcIARBFGMIDMgcIABBFGMIDMgcIARBFGMIDMgcIAhBFGMIDMgcIAxBFGMIDMgcIBBBFGMIDMgcIBRBFGMIDMgcIBhBFGMIDMgcIBxBFGMID0gENNTgyOTgzNzYwajBqMagCCLACAQ&FORM=ANNTA1&ucpdpc=UCPD&adppc=EDGEESS&PC=U531");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public ContentResult Content()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Content("<h1>Hello From Gym Management System</h1>" , "text/html" );
+        }
+
+        public FileResult DownloadFile()
+        {
+            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
+            var FileBytes=System.IO.File.ReadAllBytes(FilePath);
+            return File(FileBytes ,"text/css" , "DownloadableSite.css"  );
+        }
+
+        public EmptyResult EmptyResult()
+        {
+            return new EmptyResult();
         }
     }
 }
+
