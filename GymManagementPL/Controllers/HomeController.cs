5f6a1f9070aa7e8f8f96eb86b90a8243a40bbbc9
rahma3laa <rahmaaaliii55@gmail.com>
2025-10-19 16:44:41 +0300

    V13

--- GymManagementPL/Controllers/HomeController.cs
+++ GymManagementPL/Controllers/HomeController.cs
@@ -6,43 +6,12 @@ namespace GymManagementPL.Controllers
     public class HomeController : Controller
     {
         //ActionResult
-        public ViewResult Index()
+        public ActionResult Index()
         {
-          return View();
+            return View();
 
         }
 
-        public JsonResult Trainers()
-        {
-            var Trainers = new List<Trainer>()
-            {
-                new Trainer(){ Name ="Rahma" , Phone ="010060"},
-                new Trainer(){ Name ="Ali" , Phone ="01006096175"},
-            };
-            return Json(Trainers);
-        }
-
-        public RedirectResult Redirect()
-        {
-            return Redirect("https://www.bing.com/search?q=download+github+desktop&gs_lcrp=EgRlZGdlKgcIARBFGMIDMgcIABBFGMIDMgcIARBFGMIDMgcIAhBFGMIDMgcIAxBFGMIDMgcIBBBFGMIDMgcIBRBFGMIDMgcIBhBFGMIDMgcIBxBFGMID0gENNTgyOTgzNzYwajBqMagCCLACAQ&FORM=ANNTA1&ucpdpc=UCPD&adppc=EDGEESS&PC=U531");
-        }
-
-        public ContentResult Content()
-        {
-            return Content("<h1>Hello From Gym Management System</h1>" , "text/html" );
-        }
-
-        public FileResult DownloadFile()
-        {
-            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
-            var FileBytes=System.IO.File.ReadAllBytes(FilePath);
-            return File(FileBytes ,"text/css" , "DownloadableSite.css"  );
-        }
-
-        public EmptyResult EmptyResult()
-        {
-            return new EmptyResult();
-        }
     }
 }
 
